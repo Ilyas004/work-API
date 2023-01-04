fc0c822da50aa5704ae02d51d986c30dc2a6cf68
Ilyas004 <103751598+Ilyas004@users.noreply.github.com>
2023-01-04 16:50:39 +0300

    Update RetrofitInstance.kt

--- app/src/main/java/com/example/mars/data/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/mars/data/api/RetrofitInstance.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     private val retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("https://api.monobank.ua/") // input name server
+            .baseUrl("Input find api server") // input name server
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
